34d883dd822b0464d8016ebc3d7ccee3e229fa11
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-12-27 23:37:07 -0500

    initial function and planning

--- test.py
+++ test.py
@@ -1,21 +1,8 @@
-import csv
-import tkinter as tk       # sudo apt install python3-tk
-from tkinter.filedialog import askopenfilename
-
-### Import base seq from file
-tk.Tk().withdraw()      # part of the import if not using other tkinter f(x)
-
-#filename = askopenfilename()
-#inputfile ="\path\to\DNA_sequence_placeholder.csv" # use for testing
-#file = open(filename, "r")
-#seq = file.read()
-
 ### Variables
-startTag = 'ATG'
+startTag = 'M' # DNA bp=ATG
 #startTag = input("Enter custom start tag:") # override default
 endTag = 'EQKLISEEDL'
 #endTag = input("Enter custom end tag:") # override default
-
 rawInput = 'atgataggcatccatcggcttgctatgcttcgcgaccgtccctatgatcgcggcgccgaccaaaactgcgctcgtcaacgtatgcgtgccatgatgatgcaacatattaacttcagtacttggcccaactccatagcgagccattatgacattagtcatgtcccaagggctttccttgcaggtattgtatcttggacttcgcaggctaggagacatataagtcgcattcgtgtcgcggaaacaaaagatattgattgcttgatggctacgttcggctgtgacagggtcacaacttcaaccaaggcaattagcgccggactgcatttactcggatttcgggacaaaattcccgcagtgcgcagtcccttagtaccgccggataagcacggccgctcctttagctttcatgcttcggttgtactacctcaacataaactcccgtcccgcgtaggagtcgcaatcgtccgcatcgacccagcaccctgtgacgtggcaaacccgagtcgtttagaaaacgaagcaagtaatgagaagcagaactggattgcgtcatgcgcaggggcggacctttttcaagtgtcacaagcgacctgccttggtgcgtgtgcttccaataggttaatttcaggaagcactctgtattggattaggaggagttggatgaccagagacccgggtaatccattaaccctgagtgttgcaggattacgggttctggccacatatctagagaaaggggttttggagccaccaaaaatgagacgacatggtacgattgaggcaggctttttaaatgtaatgaaccccaaacaacagcgggtgaaacgatacacttcaagggctattttgcacgtgggatgttataaatcaggcctgcagggtttcgatagagtgtgtaccaacgccttccccccggccaagatcgtttccagggtattcgcagaacaaaaactcatctcagaagaggatctgtga'
 seq = rawInput.upper()
 ### Read seq content to upper list
@@ -38,3 +25,6 @@ print(startIndex)
 
 ## Read from start to stop codon
 print(firstFind(seq, endTag, startIndex))
+
+
+
